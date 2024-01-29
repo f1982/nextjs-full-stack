278d1882dadd9038ca554edb0b876013d6c6c860
Andy <caojundan@gmail.com>
2024-01-29 20:35:45 +1300

    fix: session

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -27,20 +27,6 @@ export const authOptions: NextAuthOptions = {
     // When using `"database"`, the session cookie will only contain a `sessionToken` value,
     // which is used to look up the session in the database.
     strategy: "jwt",
-
-    // Seconds - How long until an idle session expires and is no longer valid.
-    maxAge: 30 * 24 * 60 * 60, // 30 days
-
-    // Seconds - Throttle how frequently to write to database to extend a session.
-    // Use it to limit write operations. Set to 0 to always update the database.
-    // Note: This option is ignored if using JSON Web Tokens
-    updateAge: 24 * 60 * 60, // 24 hours
-
-    // The session token is usually either a random UUID or string, however if you
-    // need a more customized session token string, you can define your own generate function.
-    generateSessionToken: () => {
-      return randomUUID?.() ?? randomBytes(32).toString("hex");
-    },
   },
 };
 
