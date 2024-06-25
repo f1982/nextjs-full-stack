2ac156f7bf752e24707a4e95d414ce4f595232be
Andy <caojundan@gmail.com>
2024-06-25 22:12:36 +1200

    feat: add hmac

--- src/app/api/openai/route.ts
+++ src/app/api/openai/route.ts
@@ -1,3 +1,4 @@
+import CryptoJS from 'crypto-js'
 import { NextRequest, NextResponse } from 'next/server'
 import OpenAI from 'openai'
 import { ChatCompletionCreateParams } from 'openai/resources'
@@ -16,7 +17,62 @@ export async function OPTIONS(req: NextRequest) {
   return new Response('', { status: 200 })
 }
 
+const SECRET_KEY = 'your-secret-key'
+
+function getClientSignature(message: string) {
+  // const timestamp = new Date().toISOString()
+  const timestamp = '2024-06-25T09:37:12.784Z'
+  const hmacClient = CryptoJS.HmacSHA256(
+    `${message}:${timestamp}`,
+    SECRET_KEY,
+  ).toString()
+  console.log('client timestamp:', timestamp)
+  console.log('client hmac:', hmacClient)
+
+  return hmacClient
+}
 export async function POST(req: NextRequest) {
+  const message = 'your-message-to-sign'
+  // getClientSignature(message)
+
+  const signature = req.headers.get('x-signature')
+  const timestamp = req.headers.get('x-timestamp')
+
+  console.log('header timestamp: ', timestamp)
+  console.log('header signature: ', signature)
+
+  if (!signature || !timestamp) {
+    return NextResponse.json({
+      status: 403,
+      data: null,
+      message: 'Missing headers',
+    })
+  }
+
+  // Validate timestamp (e.g., allow a 5-minute window)
+  const requestTime = new Date(timestamp).getTime()
+  const currentTime = new Date().getTime()
+  if (currentTime - requestTime > 5 * 60 * 1000) {
+    return NextResponse.json({
+      status: 403,
+      data: null,
+      message: 'Request expired',
+    })
+  }
+
+  const hmac = CryptoJS.HmacSHA256(
+    `${message}:${timestamp}`,
+    SECRET_KEY,
+  ).toString(CryptoJS.enc.Hex)
+
+  if (hmac !== signature) {
+    return NextResponse.json({
+      status: 403,
+      data: null,
+      message: 'Invalid signature',
+    })
+  }
+
   const { prompt, imageUrl } = await req.json()
 
   const params: ChatCompletionCreateParams = {
@@ -38,10 +94,5 @@ export async function POST(req: NextRequest) {
     ],
   }
   const response = await openai.chat.completions.create(params)
-  // return response.choices[0].message
   return NextResponse.json({ status: 'success', data: response })
-  // Convert the response into a friendly text-stream
-  // const stream = OpenAIStream(response)
-  // Respond with the stream
-  // return new StreamingTextResponse(stream)
 }
